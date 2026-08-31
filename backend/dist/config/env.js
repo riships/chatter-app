409b7b371901e5302e94d7108a6b043ad8691d90
riships <avviare.rishi@gmail.com>
2026-08-31 16:32:25 +0530

    refactor: remove unused project configuration files

--- backend/dist/config/env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-export const config = {
-    PORT: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
-    DB_URI: process.env.DB_URI || process.env.DBURI || 'mongodb://127.0.0.1:27017/chat-app',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || '*',
-    NODE_ENV: process.env.NODE_ENV || 'development',
-};
-export default config;
